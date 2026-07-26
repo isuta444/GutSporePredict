c4df513a914c9edcb768eca056130eceb3dcf4b3
Atsushi Hisatomi <isuta444@gmail.com>
2026-07-26 16:18:02 +0900

    refactor(io): clarify supported genome FASTA files

--- src/gutsporepredict/io/genome_loader.py
+++ src/gutsporepredict/io/genome_loader.py
@@ -4,36 +4,27 @@ from pathlib import Path
 
 from gutsporepredict.models.genome import Genome
 
-class GenomeLoader:
 
+class GenomeLoader:
     """Load genome FASTA files from a directory."""
 
     VALID_SUFFIXES = {".fa", ".fna", ".fasta"}
 
     def __init__(self, genome_dir: str | Path):
-
         self.genome_dir = Path(genome_dir)
 
     def load(self) -> list[Genome]:
-
         genomes = []
 
         for fasta in sorted(self.genome_dir.iterdir()):
-
             if fasta.suffix.lower() not in self.VALID_SUFFIXES:
-
                 continue
 
             genomes.append(
-
                 Genome(
-
                     accession=fasta.stem,
-
                     fasta=fasta,
-
                 )
-
             )
 
         return genomes
